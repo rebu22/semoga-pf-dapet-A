82bd6ccee25fdae2a4b21c10029d9facaeed323b
rebu22 <pasyapahlev129@gmail.com>
2026-06-04 23:06:31 +0800

    first time

---
+++ tubes_sieve_python.py
@@ -0,0 +1,32 @@
+N = 2_000_000
+prima = [True] * (N + 1)
+
+prima[0] = False
+prima[1] = False
+
+import math
+limit = int(math.sqrt(N))
+
+import time
+
+start_time = time.perf_counter() 
+# ... (proses nested loop) ...
+end_time = time.perf_counter()    
+
+duration = (end_time - start_time) * 1_000_000
+
+i = 2
+while i <= limit:
+    if prima[i] == True:
+        # Loop dalam dimulai dari i * i
+        j = i * i
+        while j <= N:
+            prima[j] = False
+            j = j + i  # Lompat sejauh kelipatannya (Logika Benar)
+    i = i + 1
+
+count = prima[2:].count(True)
+
+print(f"Batas N: {N}")
+print(f"Jumlah bilangan prima: {count}")
+print(f"Waktu eksekusi: {duration:.2f} mikrodetik")
