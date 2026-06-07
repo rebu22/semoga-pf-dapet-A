6bba145851801b1b3655c113a7005bb65484f92e
rebu22 <pasyapahlev129@gmail.com>
2026-06-07 15:47:14 +0800

    revisi python

--- python/tubes_sieve_python.py
+++ python/tubes_sieve_python.py
@@ -4,18 +4,10 @@ prima = [True] * (N + 1)
 prima[0] = False
 prima[1] = False
 
-import math
-limit = int(math.sqrt(N))
-
 import time
 
-start_time = time.perf_counter() 
-end_time = time.perf_counter()    
-
-duration = (end_time - start_time) * 1_000_000
-
 i = 2
-while i <= limit:
+while i*i <= N:
     if prima[i] == True:
         j = i * i
         while j <= N:
@@ -23,9 +15,14 @@ while i <= limit:
             j = j + i  
     i = i + 1
 
+start_time = time.perf_counter() 
+end_time = time.perf_counter()  
+
+duration = (end_time - start_time) * 1_000_000
+
 count = prima[2:].count(True)
 
-print("Bilangan prima hingga", N, ":")
+print("Bilangan prima", ":")
 print(f"Batas N: {N}")
 print(f"Jumlah bilangan prima: {count}")
 print(f"Waktu eksekusi: {duration:.2f} ms")
